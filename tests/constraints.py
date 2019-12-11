44a071b522d12d90f75f7f5c2925e24a77f3fa99
Ben Milnes <benjamin.milnes@nagwa.com>
2019-12-11 15:24:55 +0000

    262 tests; all passing

--- tests/constraints.py
+++ tests/constraints.py
@@ -35,3 +35,27 @@ mustHaveExactly3SF = {
 mustHaveExactly5SF = {
     "mustHaveExactlyNSF": 5
 }
+
+mustHaveAtLeast3DP = {
+    "mustHaveAtLeastNDP": 3
+}
+
+mustHaveAtLeast5DP = {
+    "mustHaveAtLeastNDP": 5
+}
+
+mustHaveNoMoreThan3DP = {
+    "mustHaveNoMoreThanNDP": 3
+}
+
+mustHaveNoMoreThan6DP = {
+    "mustHaveNoMoreThanNDP": 6
+}
+
+mustHaveExactly3DP = {
+    "mustHaveExactlyNDP": 3
+}
+
+mustHaveExactly5DP = {
+    "mustHaveExactlyNDP": 5
+}
